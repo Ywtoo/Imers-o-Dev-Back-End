80712136be91c7a3df7ff2ba90f93fd92b21400c
Gabriel Nascimento <gabrieln99626@gmail.com>
2024-11-25 11:41:15 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+//npm init es6 -y
+import express from "express";
+import routes from "./src/routes/postRoutes.js";
+
+const app = express();
+app.use(express.static('uploads'));
+routes(app)
+
+app.listen(3000, () => {
+    console.log("Servidor escutando");
+});
+
+//http.cat
